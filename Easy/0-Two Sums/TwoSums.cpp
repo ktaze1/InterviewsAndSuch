32292885330be13fdbb8264ff2c6e544cff0dfa9
Kaan Taze <bkaantaze@gmail.com>
2020-06-20 15:56:08 +0300

    added TwoSums Brute Force

--- Easy/0-Two Sums/TwoSums.cpp
+++ Easy/0-Two Sums/TwoSums.cpp
@@ -1,7 +1,43 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
 #include <iostream>
+#include <vector>
+
+
+
+std::vector<int> twoSum(std::vector<int>& nums, int target){
+    std::vector<int> result;
+    
+    for(auto i{0}; i<nums.size() -1; i++){
+        for(auto j{1}; j<nums.size(); j++){
+            if(nums[i] + nums[j] == target){
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+            }
+        }
+    }
+    return result;
+}
+
 
 int main()
 {
-    std::cout << "Hello World \n";
+    std::vector<int> answer {0};
+    std::vector<int> test{2,7,11,15};
+    answer = twoSum(test, 26);
+    for(int i{0}; i < answer.size(); i++){
+        std::cout << answer[i];
+    }
     return 0;
 }
