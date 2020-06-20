92d1404bf95c4224b0e8ca9361f430b3dd6c2828
Kaan Taze <bkaantaze@gmail.com>
2020-06-21 00:39:47 +0300

    added longest happy str

---
+++ Medium/1-Longest string without 3 consecutive chars/longestHappyString.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+std::string longestDiverseString(int a, int b, int c) 
+{
+    //using max heap
+    std::priority_queue<std::pair<int,char>>pq;
+    if(a)
+    pq.push({a,'a'});
+    if(b)
+    pq.push({b,'b'});
+    if(c)
+    pq.push({c,'c'});
+    std::string ans="";
+    while(pq.size()>1){
+        std::pair<int,char>one = pq.top();pq.pop();
+        std::pair<int,char>two = pq.top();pq.pop();
+        
+        ans+=std::string(one.first>=2? 2:1,one.second);
+        one.first-=one.first>=2? 2:1;
+        
+        ans+=std::string((two.first>=2 && two.first>=one.first)? 2:1,two.second);
+        two.first-=(two.first>=2 && two.first>=one.first)? 2:1;
+
+        if(one.first>0) pq.push(one);
+        if(two.first>0) pq.push(two);
+    }
+    
+    if(pq.empty()) return ans;
+    ans+=std::string(pq.top().first>=2? 2:1,pq.top().second);
+
+    return ans;
+}
